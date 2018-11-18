75e95df0dbb909c070cd87a12b5fc1434faa191f
anshuman_hitcher <anshuman.acharya04@gmail.com>
2018-11-19 09:30:15 +1100

    new changes

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -18,9 +18,9 @@ class Landing extends Component {
           <div className="container">
             <div className="row">
               <div className="col-md-12 text-center">
-                <h1 className="display-3 mb-4">Dev Space
+                <h1 className="display-3 mb-4">Alplas
             </h1>
-                <p className="lead"> Create A Developer Profile/Portfolio, Share Posts And Get Help From Other Developers</p>
+                <p className="lead"> </p>
                 <hr />
                 <Link to="/register" className="btn btn-lg btn-info mr-2">Sign Up</Link>
                 <Link to="/login" className="btn btn-lg btn-light">Login</Link>
